01a263dc6ee3866e70b53642558d19b6d9a881a2
thorbucks <145323024+thorbucks@users.noreply.github.com>
2023-09-20 23:28:36 -0700

    PANGIT MO (#7)

---
+++ bawalmabaho.py
@@ -0,0 +1,15 @@
+print("PANGIT MO")
+
+answer = input("Pangit ka ba?")
+
+if answer == "yes":
+    print("Pangit ka.")
+
+elif answer == "maybe":
+    print("Hindi ka pangit.")
+
+elif answer == "no":
+    print("Pangit ka pa rin.")
+
+else:
+    print("Mas malala ka sa pangit.")
